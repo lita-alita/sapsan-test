c3e6f6442ff6733bf5fbd219ae81fa0dc1c58bf7
lita-alita <mrslitik@gmail.com>
2023-11-16 18:34:00 +0300

    add spiner on loading

--- src/components/query-results/index.tsx
+++ src/components/query-results/index.tsx
@@ -1,23 +1,24 @@
 import ImageCard from "./image-card"
 import useSWR from "swr"
 import style from "./index.module.css"
-
+import spiner from "@/assets/spiner.svg"
+import Image from "next/image"
 interface IPropQueryResults {
     query: string
 }
 
 const fetcher = (...args: any) => fetch(args).then(res => res.json())
 
-function useQuery (query: string | null) {
+function useQuery(query: string | null) {
     const { data, error, isLoading } = useSWR(query ? "https://api.unsplash.com/search/photos?client_id=Ip0XA55zY7b7-d19osq1L5btGg-YCeDZVpnnJjXqHxs&page=1&lang=ru&per_page=24&query=" + query : null, fetcher)
-   
+
     return {
-      result: data,
-      isLoading,
-      isError: error
+        result: data,
+        isLoading,
+        isError: error
     }
-  }
-  
+}
+
 export default function QueryResults({ query }: IPropQueryResults) {
     const { result, isLoading, isError } = useQuery(query === "" ? null : query)
 
@@ -28,5 +29,14 @@ export default function QueryResults({ query }: IPropQueryResults) {
             })}
         </div>
     )
+
+    if (isLoading) {
+        return (
+            <span>
+                <Image className={style.spiner} src={spiner} alt="" width={32} height={32} />
+            </span>
+
+        )
+    }
     return <div></div>
 }
