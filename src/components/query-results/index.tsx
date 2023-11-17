6013b254f291630b08567825f2fd45a16abea956
lita-alita <mrslitik@gmail.com>
2023-11-17 18:13:03 +0300

    add infinity scroll

--- src/components/query-results/index.tsx
+++ src/components/query-results/index.tsx
@@ -6,31 +6,69 @@ import style from "./index.module.css"
 import spiner from "@/assets/spiner.svg"
 import Image from "next/image"
 import Pagination from "./pagination"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { flushSync } from 'react-dom';
 
 interface IPropQueryResults {
     query: string
+}
+
+interface IPropResult {
+    result: any,
+    isLoading: boolean,
+    isError: boolean
 }
 
 const fetcher = (...args: any) => fetch(args).then(res => res.json())
 
 export default function QueryResults({ query }: IPropQueryResults) {
     const [page, setPage] = useState(1)
-    const { result, isLoading, isError } = useQuery(query === "" ? null : query)
+    const [data, setData] = useState<any[]>([])
+    const { result, isLoading, isError } = useQuery(query === "" ? null : `https://api.unsplash.com/search/photos?client_id=Ip0XA55zY7b7-d19osq1L5btGg-YCeDZVpnnJjXqHxs&page=${page}&lang=ru&per_page=24&query=${query}`)
 
-    function useQuery (query: string | null) {
-        const { data, error, isLoading } = useSWR(query ? `https://api.unsplash.com/search/photos?client_id=Ip0XA55zY7b7-d19osq1L5btGg-YCeDZVpnnJjXqHxs&page=${page}&lang=ru&per_page=24&query=${query}` : null, fetcher)
+    useEffect(() => {
+        window.addEventListener("scroll", ()=>scrollHandler(page))
+        return function () {
+            window.removeEventListener("scroll", ()=>scrollHandler(page))
+        }
+    }, [])
 
+    useEffect(() => {
+        if (result) setData([...data, ...result?.results])
+    }, [result, page])
+
+    function useQuery(query: string | null): IPropResult {
+        const { data, error, isLoading } = useSWR(query, fetcher)
         return {
             result: data,
             isLoading,
             isError: error
         }
     }
+
+    const scrollHandler = (pageCurrent:number) => {
+        let height = document.body.offsetHeight
+        let screenHeight = window.innerHeight
+        let scrolled = window.scrollY
+
+        const position = scrolled + screenHeight
+
+        const threshold = height - screenHeight / 4
+        if (position >= threshold) {
+            flushSync(()=>setPage((page)=>page+1))
+        }
+    }
+
     if (result) return (
         <span>
-            <div className={style.queryResults} id="queryResults">
-                {result.results.map((res: any) => {
+            <div
+                className={style.queryResults}
+                id="queryResults"
+            // onScroll={(e) => {
+            //     scrollHandler()
+            // }}
+            >
+                {data.map((res: any) => {
                     return <ImageCard src={res.urls.full} alt={res.alt_description} miniSrc={res.urls.small} key={res.id} />
                 })}
             </div>
